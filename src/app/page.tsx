3f12c1823aa95b80c5ee09415551c2e24bbbaf5e
Chip Wiley <chipwiley@outlook.com>
2025-01-25 20:58:31 -0800

    Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,9 @@ export default function Home() {
   return (
     <div ref={container} className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 style={{opacity: 1, textTransform: "uppercase"}} className="message text-4xl">Build What's Next...</h1>
-        {/* <h1  style={{opacity: 0, fontSize: "200px"}} className="end text-4xl text-center self-center">!</h1> */}
+        
         <Image
-          className="end dark:invert self-center"
+          className="end dark:invert self-center "
           style={{opacity: 0}}
           src="/next.svg"
           alt="Next.js logo"
@@ -56,6 +55,12 @@ export default function Home() {
           height={38}
           priority
         />
+        
+        <h1 
+          style={{opacity: 1, textTransform: "uppercase"}} 
+          className="message text-4xl">Build What's Next...
+        </h1>
+
       </div>
     </div>
   );
