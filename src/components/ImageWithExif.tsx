15b956d5a2eb004ed11ae6a294700ceb2c2d8f54
Chip Wiley <chipwiley@outlook.com>
2025-01-26 11:31:35 -0800

    Fade in images on load

--- src/components/ImageWithExif.tsx
+++ src/components/ImageWithExif.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export interface ExifData {
     make?: string;
@@ -20,9 +21,18 @@ interface ImageWithExifProps {
     exifData: ExifData;
 }
 
+
 export const ImageWithExif = ({ src, alt, exifData }: ImageWithExifProps) => {
     const overlayRef = useRef<SVGSVGElement>(null);
     const [isHovering, setIsHovering] = useState(false);
+
+    const imageRef = useRef<HTMLImageElement>(null);
+
+    useGSAP(() => {
+        if (imageRef.current) {
+            gsap.to(imageRef.current,{ opacity: 1, duration: 0.5 });
+        }
+    }, []);
 
     useEffect(() => {
         if (!overlayRef.current) return;
@@ -50,9 +60,11 @@ export const ImageWithExif = ({ src, alt, exifData }: ImageWithExifProps) => {
                 onMouseLeave={() => setIsHovering(false)}
             >
                 <img
+                    ref={imageRef}
                     src={src}
                     alt={alt}
                     className="w-full h-auto object-cover aspect-[4/3] rounded-xl"
+                    style={{ opacity: 0 }}
                 />
                 <svg
                     ref={overlayRef}
@@ -73,7 +85,7 @@ export const ImageWithExif = ({ src, alt, exifData }: ImageWithExifProps) => {
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path fill="#FFD700" d="M21 6h-4.18C16.4 4.84 14.98 4 13.5 4c-1.48 0-2.9.84-3.32 2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h15c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7.5 11c-2.49 0-4.5-2.01-4.5-4.5S11.01 8 13.5 8s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-7c-1.38 0-2.5 1.12-2.5 2.5S12.12 15 13.5 15s2.5-1.12 2.5-2.5S14.88 10 13.5 10z"/>
+                        <path fill="#FFD700" d="M21 6h-4.18C16.4 4.84 14.98 4 13.5 4c-1.48 0-2.9.84-3.32 2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h15c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7.5 11c-2.49 0-4.5-2.01-4.5-4.5S11.01 8 13.5 8s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-7c-1.38 0-2.5 1.12-2.5 2.5S12.12 15 13.5 15s2.5-1.12 2.5-2.5S14.88 10 13.5 10z" />
                     </svg>
                     <text
                         x="20"
@@ -109,16 +121,16 @@ export const ImageWithExif = ({ src, alt, exifData }: ImageWithExifProps) => {
 
             </div>
             <div style={{ color: '#FFD700', fontSize: '14px' }} className=" bottom-0 right-0 p-2 text-right text-white">
-               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                   <div style={{ display: 'flex', alignItems: 'center' }}>
-                       
-                       <svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                           <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z" fill="#FFD700"/>
-                       </svg>
-                       <div>{exifData.location}</div>
-                   </div>
-                   <div>{exifData.dateTaken ? new Date(exifData.dateTaken).toLocaleDateString() : 'Unknown Date'}</div>
-               </div>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+
+                        <svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z" fill="#FFD700" />
+                        </svg>
+                        <div>{exifData.location}</div>
+                    </div>
+                    <div>{exifData.dateTaken ? new Date(exifData.dateTaken).toLocaleDateString() : 'Unknown Date'}</div>
+                </div>
             </div>
 
         </>
